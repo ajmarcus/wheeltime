0f5de011e18a648f36d4bf8190f2f5579b55c102
Ariel Marcus <git@arielmarcus.com>
2013-12-11 01:55:00 -0500

    added static files

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Wheeltime
 ========
 
 Realtime flask app through long polling.
+
+Sine wave generation borrowed from:
+http://zacharydenton.com/generate-audio-with-python/
+
+Static files borrowed from:
+https://github.com/heroku-examples/python-websockets-chat
--- wheeltime.py
+++ wheeltime.py
@@ -1,23 +1,12 @@
 #!/usr/local/bin/python
 
-from flask import Flask, jsonify
 import math
+from flask import Flask, jsonify, render_template
 from itertools import count
 app = Flask(__name__)
 app.config.from_object('config')
 
-@app.route('/')
-def hello_world():
-	return 'Hello World!'
-
-@app.route('/roll')
-def roll():
-	result = {
-		'finserv':finserv.next(),
-		'tech':tech.next()
-		}
-	return jsonify(result)
-
+# App
 
 # From http://zacharydenton.com/generate-audio-with-python/
 def sine_wave(frequency=440.0, framerate=44100, amplitude=0.5):
@@ -28,10 +17,24 @@ def sine_wave(frequency=440.0, framerate=44100, amplitude=0.5):
 	if amplitude > 1.0: amplitude = 1.0
 	if amplitude < 0.0: amplitude = 0.0
 	lookup_table = [float(amplitude) * math.sin(2.0*math.pi*float(frequency)*(float(i%period)/float(framerate))) for i in xrange(period)]
-	return (lookup_table[i%period] for i in count(0))
+	return (lookup_table[i%period]+1 for i in count(0))
 
 finserv = sine_wave(amplitude=0.6)
 tech = sine_wave()
+
+# Routes
+
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+@app.route('/roll')
+def roll():
+	result = {
+		'finserv':finserv.next(),
+		'tech':tech.next()
+		}
+	return jsonify(result)
 
 if __name__ == '__main__':
 	app.run()
